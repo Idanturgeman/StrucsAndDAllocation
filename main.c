c453c4172ab0337ceac045f62168a9a80f904138
Idanturjeman <idan_turjeman29@walla.co.il>
2021-01-10 14:36:27 +0200

    add main

---
+++ main.c
@@ -0,0 +1,34 @@
+#include "stringFunctions.h"
+#include "trie.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char const *argv[]) {
+  char* word = NULL;
+  Trie trie;
+
+  if(constructTrie(&trie) == ERROR)
+    return ERROR;
+
+  int lent;
+  do {
+    lent = getUnlimitWord(&word);
+    if(lent > 0){
+      if(insertWord(&trie, word, lent) == ERROR){
+        printf("insertWord failed\n");
+        return ERROR;
+      }
+    }
+    if(word != NULL)
+      free(word);
+  } while(lent != EOF);
+
+  bool rvrs = (argc > 1 && *(argv[1]) == 'r');
+
+  printTrie(&trie, rvrs);
+  destroyTrie(&trie);
+
+  return 0;
+}
